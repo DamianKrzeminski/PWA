02ade1b681424504595f721b77095e595684fe39
Damian Krzemiński <damian.krzeminski13@gmail.com>
2023-04-22 14:36:31 +0200

    search

--- pages/nasa/index.vue
+++ pages/nasa/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <h1>Nasa {{ message }} </h1>
-        <input type="text" v-model="message" placeholder="search...">
+        <h1>Nasa {{ lookingForItem }} </h1>
+        <input type="text" v-model="lookingForItem" placeholder="search...">
         <button @click="$event = find()">Search</button>
         <div class="img-nasa-box">
-            <img class="image-nasa" :src="value.links[0].href" v-for="(value, index) in dataArr" :key="index"/>
+            <img class="image-nasa" @click="$event = makeBigger()" :src="value.links[0].href" v-for="(value, index) in dataArr" :key="index"/>
         </div>
     </div>
 </template>
@@ -14,17 +14,20 @@ import axios from "axios";
 export default {
     name: "index",
     created(){
-        this.fetchData(this.message)
+        this.fetchData(this.lookingForItem)
     },
     data(){
         return{
             dataArr: [],
-            message: "sun"
+            lookingForItem: "sun"
         }
     },
     methods:{
         find(){
-            this.fetchData(this.message);
+            this.fetchData(this.lookingForItem);
+        },
+        makeBigger(){
+            
         },
         async fetchData(search){
             await axios.get('https://images-api.nasa.gov/search?q=' + search)
